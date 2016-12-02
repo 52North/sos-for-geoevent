1eff2ad06e368c10b8b45ecd4f3243abd0207f9a
SebaDro <sebastian.drost@hs-bochum.de>
2016-12-02 13:59:28 +0100

    Add functionality for requesting only new sensor data
    Add functionality for requesting only new sensor data and a property for
    defining an initial request

--- sos-transport/src/main/java/org/n52/sensorweb/sos/transport/SOSInboundTransport.java
+++ sos-transport/src/main/java/org/n52/sensorweb/sos/transport/SOSInboundTransport.java
@@ -1,5 +1,6 @@
 package org.n52.sensorweb.sos.transport;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,12 +9,17 @@ import java.net.URISyntaxException;
 import java.nio.BufferOverflowException;
 import java.nio.ByteBuffer;
 
+import javax.xml.bind.JAXBException;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 import com.esri.ges.core.component.ComponentException;
 import com.esri.ges.core.component.RunningException;
@@ -22,6 +28,8 @@ import com.esri.ges.framework.i18n.BundleLogger;
 import com.esri.ges.framework.i18n.BundleLoggerFactory;
 import com.esri.ges.transport.InboundTransportBase;
 import com.esri.ges.transport.TransportDefinition;
+
+import net.opengis.om.x10.ObservationCollection;
 
 public class SOSInboundTransport extends InboundTransportBase implements Runnable {
 	/**
@@ -37,23 +45,39 @@ public class SOSInboundTransport extends InboundTransportBase implements Runnabl
 	private final String OBSERVED_PROPERTY_KEY = "observedProperty";
 	private final String PROCEDER_KEY = "procedure";
 	private final String RESPONSE_FORMAT_KEY = "responseFormat";
+	private final String EVENT_TIME_KEY = "eventTime";
 
 	private final String REQUEST_VALUE = "GetObservation";
 	private final String SERVICE_VALUE = "SOS";
 	private final String VERSION_VALUE = "1.0.0";
 	private final String RESPONSE_FORMAT_XML = "text/xml;subtype=\"om/1.0.0\"";
+	private final ObservationUnmarshaller observationParser;
+	private final int EVENT_TIME_OFFSET = 1;
 
 	private String url;
 	private String offering;
 	private String observedProperty;
 	private String procedure;
 	private int requestInterval;
+	private int nDaysInitialRequest;
+	private DateTime eventTimeBegin;
+	private URI requestURI;
 	private HttpGet httpGet;
 
 	private Thread thread = null;
 
 	public SOSInboundTransport(TransportDefinition definition) throws ComponentException {
 		super(definition);
+		try {
+			LOGGER.info("Create observation parser");
+			observationParser = new ObservationUnmarshaller();
+			LOGGER.info("Observation Parser Created");
+		} catch (JAXBException e) {
+			LOGGER.warn(e.getMessage(), e);
+			LOGGER.warn("Ok Fehler");
+			throw new ComponentException(e.getMessage());
+		}
+		eventTimeBegin = null;
 	}
 
 	public void applyProperties() throws Exception {
@@ -96,6 +120,14 @@ public class SOSInboundTransport extends InboundTransportBase implements Runnabl
 				requestInterval = value * 1000;
 			}
 		}
+
+		nDaysInitialRequest = 3; // default
+		if (getProperty("eventTimeBegin").isValid()) {
+			int value = (Integer) getProperty("eventTimeBegin").getValue();
+			if (value > 0 && value != nDaysInitialRequest) {
+				nDaysInitialRequest = value;
+			}
+		}
 	}
 
 	@SuppressWarnings("incomplete-switch")
@@ -120,8 +152,13 @@ public class SOSInboundTransport extends InboundTransportBase implements Runnabl
 		try {
 			applyProperties();
 			setRunningState(RunningState.STARTED);
-			httpGet = createHttpGet();
+			if (eventTimeBegin == null) {
+				eventTimeBegin = DateTime.now().minusDays(nDaysInitialRequest);
+			}
+			requestURI = createHttpURI();
 			while (getRunningState() == RunningState.STARTED) {
+				setURIEventTimeParameter();
+				httpGet = new HttpGet(requestURI);
 				receiveData();
 				Thread.sleep(requestInterval);
 			}
@@ -154,11 +191,17 @@ public class SOSInboundTransport extends InboundTransportBase implements Runnabl
 			InputStream inStream = entity.getContent();
 
 			byte[] data = getByteArrayFromInputStream(inStream);
-			bb = ByteBuffer.allocate(data.length);
-			bb.put(data);
-			bb.flip();
-			byteListener.receive(bb, "");
-			bb.clear();
+
+			ObservationCollection collection = getObservationCollection(new ByteArrayInputStream(data));
+			if (hasEventValues(collection)) {
+				eventTimeBegin = getLatestSamplingTime(collection).plusSeconds(EVENT_TIME_OFFSET);
+				bb = ByteBuffer.allocate(data.length);
+				bb.put(data);
+				bb.flip();
+				LOGGER.info("SEND BYTE DATA");
+				byteListener.receive(bb, "");
+				bb.clear();
+			}
 
 		} catch (IOException e) {
 			LOGGER.error(e.getMessage());
@@ -213,10 +256,10 @@ public class SOSInboundTransport extends InboundTransportBase implements Runnabl
 	 * 
 	 * @return GetObservation request as HTTP-GET
 	 */
-	private HttpGet createHttpGet() {
-		HttpGet httpGet = null;
+	private URI createHttpURI() {
 		String urlPrefix = "http://";
 		URI uri;
+		URI fullUri = null;
 		try {
 			URIBuilder builder = new URIBuilder();
 
@@ -229,17 +272,69 @@ public class SOSInboundTransport extends InboundTransportBase implements Runnabl
 				uri = new URI(sb.toString());
 			}
 			// Create the URI for the request with all parameters
-			URI fullUri = builder.setScheme("http").setHost(uri.getHost()).setPath(uri.getPath())
+			fullUri = builder.setScheme("http").setHost(uri.getHost()).setPath(uri.getPath())
 					.setParameter(REQUEST_KEY, REQUEST_VALUE).setParameter(SERVICE_KEY, SERVICE_VALUE)
 					.setParameter(VERSION_KEY, VERSION_VALUE).setParameter(OFFERING_KEY, offering)
 					.setParameter(OBSERVED_PROPERTY_KEY, observedProperty).setParameter(PROCEDER_KEY, procedure)
-					.setParameter(RESPONSE_FORMAT_KEY, RESPONSE_FORMAT_XML).build();
-			httpGet = new HttpGet(fullUri);
+					.setParameter(EVENT_TIME_KEY, "").setParameter(RESPONSE_FORMAT_KEY, RESPONSE_FORMAT_XML).build();
 		} catch (URISyntaxException e) {
 			LOGGER.error(e.getMessage());
 			setRunningState(RunningState.ERROR);
 		}
-		return httpGet;
+		return fullUri;
 	}
 
+	/**
+	 * Sets the the event time parameter for the GetObservation request
+	 */
+	private void setURIEventTimeParameter() {
+		URIBuilder builder = new URIBuilder(this.requestURI);
+		DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+		String formattedEventTimeBegin = formatter.print(eventTimeBegin);
+		String formmettedEventTimeEnd = formatter.print(DateTime.now());
+		String eventTimeValue = new StringBuilder(formattedEventTimeBegin).append("/").append(formmettedEventTimeEnd)
+				.toString();
+		LOGGER.info("TimeBegin: " + formattedEventTimeBegin + " TimeEnd: " + formmettedEventTimeEnd
+				+ " EventTimeValue: " + eventTimeValue);
+		try {
+			requestURI = builder.setParameter(EVENT_TIME_KEY, eventTimeValue).build();
+			LOGGER.info("Request URI: " + requestURI.toString());
+		} catch (URISyntaxException e) {
+			LOGGER.error(e.getMessage());
+			setRunningState(RunningState.ERROR);
+		}
+	}
+
+	private boolean hasEventValues(ObservationCollection collection) {
+		if (collection.getMember().getObservation() != null)
+			return true;
+		else
+			return false;
+	}
+
+	/**
+	 * Determines the latest sampling time from the response
+	 * 
+	 * @param inStream
+	 *            InputStream that has to be parsed
+	 * @return the latest sampling time
+	 */
+	private DateTime getLatestSamplingTime(ObservationCollection collection) {
+		String latestSamplingTime = collection.getMember().getObservation().getSamplingTime().getTimePeriod()
+				.getEndPosition();
+		DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+		DateTime date = formatter.parseDateTime(latestSamplingTime);
+		return date;
+	}
+
+	private ObservationCollection getObservationCollection(InputStream inStream) {
+		ObservationCollection collection = null;
+		try {
+			collection = observationParser.readObservationCollection(inStream);
+		} catch (JAXBException e) {
+
+			LOGGER.error(e.getMessage());
+		}
+		return collection;
+	}
 }
