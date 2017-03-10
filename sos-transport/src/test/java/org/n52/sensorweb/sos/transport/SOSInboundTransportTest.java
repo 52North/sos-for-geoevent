81f8b6412156a58310e6764ab0a9ee0a271cd8c1
SebaDro <sebastian.drost@hs-bochum.de>
2017-03-10 09:11:15 +0100

    change property and definition files

--- sos-transport/src/test/java/org/n52/sensorweb/sos/transport/SOSInboundTransportTest.java
+++ sos-transport/src/test/java/org/n52/sensorweb/sos/transport/SOSInboundTransportTest.java
@@ -5,9 +5,6 @@ import java.io.InputStream;
 import javax.xml.bind.JAXBException;
 
 import org.hamcrest.CoreMatchers;
-import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -29,7 +26,7 @@ public class SOSInboundTransportTest {
 		ObservationCollection coll = unm.readObservationCollection(is);
 		Assert.assertThat(coll.getMember(), CoreMatchers.notNullValue());
 		Assert.assertThat(coll.getMember().getObservation().getSamplingTime().getTimePeriod().getEndPosition(),
-				CoreMatchers.equalTo("2016-10-27T11:15:00.000Z"));
+				CoreMatchers.equalTo("2017-03-09T23:20:00.000Z"));
 		
 	}
 }
